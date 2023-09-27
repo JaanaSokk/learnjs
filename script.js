a46fd1bc77e609664885b7bfedae5779f566d6f1
Jaana-Liis Sokk <jaana-liis.sokktptlive.ee>
2023-09-27 14:06:01 +0300

    Add js number and string example

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('hello world!');
+
+let num = 1;
+num = 3;
+num = 3 * 3 + 7 / 2 - 15;
+num = num + 3;
+num += 3;
+num ++;
+num = num**2;
+num **= 3;
+num = 13;
+num = num % 7;
+num %= 2;
+//var text = 'Hello'; Don't use var
+console.log (num);
+let string = 'hello';
+string = "hello";
+string ='hello number is: ${num}';
